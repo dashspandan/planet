0e0250ddb6f3a820778c67917737140a8fdc12c5
dashspandan <32219429+dashspandan@users.noreply.github.com>
2020-09-22 22:56:52 +0530

    Improved spacing between planets

--- nextorbit.py
+++ nextorbit.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random as rn
 
 def findnextorbit(m_star, m_planet, disk_edge, iceline): #Note disk_edge is orbit of planet before in ordinal number
 
@@ -7,11 +8,13 @@ def findnextorbit(m_star, m_planet, disk_edge, iceline): #Note disk_edge is orbi
 	m_planet = m_planet # earth masses, preferable the isolation mass
 	m_planet_kg = np.multiply(m_planet, 5.972e24)
 	disk_edge = disk_edge # in au
+	rn.seed(rn.random())
+	Klist = np.uniform(10, 15, 100).tolist()
 	factor = np.power(np.divide(m_planet_kg, 3.*m_star_kg), 1./3.)
 
 	d = disk_edge
 
-	c = 5.*d*factor
+	c = rn.choice(Klist)*d*factor
 	b = d+c
 
 
